11ca657d93c5ab1502cd246ef7b8be8cbc31fa7e
qifanxin <2511242638@qq.com>
2022-08-11 00:21:10 +0800

    允许使用apt

--- lib/route/meta/route_meta.dart
+++ lib/route/meta/route_meta.dart
@@ -1,6 +1,6 @@
-// import 'package:analyzer/dart/element/element.dart';
-// import 'package:build/build.dart';
-// import 'package:source_gen/source_gen.dart';
+import 'package:analyzer/dart/element/element.dart';
+import 'package:build/build.dart';
+import 'package:source_gen/source_gen.dart';
 
 class RouteMetaResource {
   static List<String> routeMetaImportList = [];
@@ -18,60 +18,60 @@ class RouteAutoWired {
   const RouteAutoWired();
 }
 
-// class RouteAutoWiredGenerator extends GeneratorForAnnotation<RouteAutoWired> {
-//   @override
-//   generateForAnnotatedElement(
-//       Element element, ConstantReader annotation, BuildStep buildStep) {
-//     String autoWiredText = "";
-//     RouteMetaResource.routeMetaImportList.forEach((element) {
-//       autoWiredText += "import 'package:$element';\n";
-//     });
-//     autoWiredText += "void routesAutoWired() {";
-//     RouteMetaResource.routeMetaMethodList.forEach((element) {
-//       autoWiredText += element;
-//     });
-//     autoWiredText += "}";
-//     return autoWiredText;
-//   }
-// }
-//
-// class RouteGenerator extends GeneratorForAnnotation<RouteMeta> {
-//   @override
-//   generateForAnnotatedElement(
-//       Element element, ConstantReader annotation, BuildStep buildStep) {
-//     String autoWiredText =
-//         "import 'package:flutter_router/route/route_match.dart';";
-//     if (element.kind == ElementKind.CLASS) {
-//       final String pagePackage;
-//       RouteMetaResource.routeMetaImportList.add(
-//           "${buildStep.inputId.package}/${buildStep.inputId.path.replaceFirst('lib/', '').replaceAll(".dart", ".route.g.dart")}");
-//       if (buildStep.inputId.path.contains('lib/')) {
-//         pagePackage =
-//             "package:${buildStep.inputId.package}/${buildStep.inputId.path.replaceFirst('lib/', '')}";
-//       } else {
-//         pagePackage = buildStep.inputId.path;
-//       }
-//
-//       autoWiredText += "import '$pagePackage';"
-//           "void autoRoutes${element.name}Generate() {";
-//       RouteMetaResource.routeMetaMethodList
-//           .add("autoRoutes${element.name}Generate();");
-//       autoWiredText +=
-//           "RouteMatch.getInstance().routes['${annotation.read("url").stringValue}'] = RouteHandler((parameters, context) => "
-//           "${element.name}(";
-//       for (var e in ((element as ClassElement).fields)) {
-//         autoWiredText += "parameters?['${e.name}'],";
-//       }
-//       autoWiredText += "));";
-//     }
-//     autoWiredText += "}";
-//     return autoWiredText;
-//   }
-// }
-//
-// Builder routeBuilder(BuilderOptions options) =>
-//     LibraryBuilder(RouteGenerator(), generatedExtension: ".route.g.dart");
-//
-// Builder routeAutoWiredBuilder(BuilderOptions options) =>
-//     LibraryBuilder(RouteAutoWiredGenerator(),
-//         generatedExtension: ".routes.g.dart");
+class RouteAutoWiredGenerator extends GeneratorForAnnotation<RouteAutoWired> {
+  @override
+  generateForAnnotatedElement(
+      Element element, ConstantReader annotation, BuildStep buildStep) {
+    String autoWiredText = "";
+    RouteMetaResource.routeMetaImportList.forEach((element) {
+      autoWiredText += "import 'package:$element';\n";
+    });
+    autoWiredText += "void routesAutoWired() {";
+    RouteMetaResource.routeMetaMethodList.forEach((element) {
+      autoWiredText += element;
+    });
+    autoWiredText += "}";
+    return autoWiredText;
+  }
+}
+
+class RouteGenerator extends GeneratorForAnnotation<RouteMeta> {
+  @override
+  generateForAnnotatedElement(
+      Element element, ConstantReader annotation, BuildStep buildStep) {
+    String autoWiredText =
+        "import 'package:flutter_router/route/route_match.dart';";
+    if (element.kind == ElementKind.CLASS) {
+      final String pagePackage;
+      RouteMetaResource.routeMetaImportList.add(
+          "${buildStep.inputId.package}/${buildStep.inputId.path.replaceFirst('lib/', '').replaceAll(".dart", ".route.g.dart")}");
+      if (buildStep.inputId.path.contains('lib/')) {
+        pagePackage =
+            "package:${buildStep.inputId.package}/${buildStep.inputId.path.replaceFirst('lib/', '')}";
+      } else {
+        pagePackage = buildStep.inputId.path;
+      }
+
+      autoWiredText += "import '$pagePackage';"
+          "void autoRoutes${element.name}Generate() {";
+      RouteMetaResource.routeMetaMethodList
+          .add("autoRoutes${element.name}Generate();");
+      autoWiredText +=
+          "RouteMatch.getInstance().routes['${annotation.read("url").stringValue}'] = RouteHandler((parameters, context) => "
+          "${element.name}(";
+      for (var e in ((element as ClassElement).fields)) {
+        autoWiredText += "parameters?['${e.name}'],";
+      }
+      autoWiredText += "));";
+    }
+    autoWiredText += "}";
+    return autoWiredText;
+  }
+}
+
+Builder routeBuilder(BuilderOptions options) =>
+    LibraryBuilder(RouteGenerator(), generatedExtension: ".route.g.dart");
+
+Builder routeAutoWiredBuilder(BuilderOptions options) =>
+    LibraryBuilder(RouteAutoWiredGenerator(),
+        generatedExtension: ".routes.g.dart");
